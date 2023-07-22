1880eedd42c1e067c99c00182750119b46261a7a
Emma Barlow <e.barlow159@gmail.com>
2023-07-22 20:52:16 +0100

    hover listeners

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ let board = document.getElementById("board");
 
 for (let i = 0; i < 256; i++) {
     let gridDiv = document.createElement("div");
-    gridDiv.innerHTML = "Div " + (i + 1);
+    gridDiv.innerHTML = i+1;
     board.appendChild(gridDiv);
+    gridDiv.addEventListener("mouseover", over);
+    gridDiv.addEventListener("mouseout", out);
+}
+
+function over(e) {
+    e.target.style.backgroundColor = `black`;
+}
+
+function out(e) {
+    e.target.style.backgroundColor = `white`;
 }
